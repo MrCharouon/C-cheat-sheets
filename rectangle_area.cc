0c78553a648e33642411d5ddd2e17c8d2870217e
MrCharouon <ali.khatami2009@gmail.com>
2022-03-29 23:39:34 +0430

    rectangle_area

---
+++ rectangle_area.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int w, h, s;
+	cout << "Enter With: ";
+	cin >> w;
+	cout << "Enter height: ";
+	cin >> h;
+
+	s = w * h;
+	cout << "Surface is " << s << endl ;
+	return 0;	
+}
