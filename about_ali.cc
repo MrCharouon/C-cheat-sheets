733c0a8b02a4f03a96996027c98961c66159fe7f
MrCharouon <ali.khatami2009@gmail.com>
2022-03-29 23:33:18 +0430

    about_ali

---
+++ about_ali.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  char ch='A'; int i=23; float f=18.75;
+  cout << "Hello, My name is Ali.";
+  cout << " First letter of my name is " << ch << ".";
+  cout << " My age is " << i << ".";
+  cout << " My Average is " << f << "." << endl;
+  return 0;
+}
